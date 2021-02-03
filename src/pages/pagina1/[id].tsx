ea9c1b2341e62f84776c557d4eff1a08840b701a
Renato Spakauskas <renatospaka@gmail.com>
2021-02-02 22:52:36 -0300

    feat: :sparkles: user/users SSR + product/products SSG

---
+++ src/pages/pagina1/[id].tsx
@@ -0,0 +1,18 @@
+// @flow 
+import * as React from 'react';
+import { useRouter } from 'next/dist/client/router'
+
+type Props = {
+  
+};
+const xptoId = (props: Props) => {
+  const {query} = useRouter()
+  
+  return (
+    <div>
+      <h1>Test param Id: {query.id}</h1>
+    </div>
+  );
+};
+
+export default xptoId;
